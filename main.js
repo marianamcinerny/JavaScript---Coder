160143853f4280f10051c703eeeb79af3fd055dd
Mariana Mc Inerny <mcinernymariana@gmail.com>
2024-07-03 19:30:26 -0300

    primera entrega

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-alert("holis");
-let nombre = "Mariana";
-let apellido = "Mc Inerny";
+const oficial = 954.10;
+const turista = 1526.56;
+const blue = 1405.00;
 
-console.log(nombre);
-console.log(apellido);
+function convertir() {
+    let total;
+    if (cambio === "oficial") 
+        total = monto * oficial;
+    else if (cambio === "turista")
+        total = monto * turista;
+    else
+        total = monto * blue;
+    return total;
+}
+
+let cambio = prompt("¿Qué tipo de cambio desea realizar? oficial - blue - turista")
+while ((cambio !== "oficial") && (cambio !== "turista") && (cambio !== "blue")) {
+    cambio = prompt("Opción incorrecta, posibles cambios: oficial - blue - turista");
+}
+
+let monto = prompt("Ingrese el monto a cambiar");
+let cantidadPesos = convertir();
+alert("Monto total: $" + cantidadPesos + " pesos argentinos");
